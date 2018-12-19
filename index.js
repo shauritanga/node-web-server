8f06fac85ac4cda185b9d2c07d718497d8ae4eb7
Athanas Shauritanga <curtisisaac36@gmail.com>
2018-12-19 18:16:12 +0300

    some modification

--- index.js
+++ index.js
@@ -7,23 +7,11 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 // MIDDLEWARE 2
-app.use((req, res, next) => {
-  res.render('test');
-});
 //MIDDLEWARE 3
-app.use((req, res, next) => {
-  let now = new Date().toString();
-  let log = `At ${now} a peroson visited ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile('visitors.log', log + '\n', err =>{
-    if(err) return err;
-  })
-  next();
-});
 
 
 app.get('/', (req, res) => {
-  res.send(index.html);
+  res.render('home', {title: 'Home'});
 });
 
 
